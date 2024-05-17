511961108ea28563db463b7a610f458837d2b26d
Mritunjay <mupadhyay00@gmail.com>
2024-05-17 09:43:52 +0700

    feat: Add s3

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -1,8 +1,8 @@
 import { S3 } from '../lib/s3.js';
 import { PutObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { ApiResponse } from "../utils/ApiResponse.utils.js";
-import { asyncHandler } from "../utils/asyncHandler.utils.js";
+import { ApiResponse } from "../utils/response.utils.js";
+import { asyncHandler } from "../utils/async.utils.js";
 
 const slugifyString = (str) => {
     return str.trim().toLowerCase().replace(/\s+/g, '-').replace(/-+/g, '-').replace(/[^a-z0-9-.]/g, '-');
@@ -11,9 +11,13 @@ const slugifyString = (str) => {
 const getSignedUrlFunc = asyncHandler(async (req, res) => {
   
     const { fileName, fileType } = req.body;    
- 
+    console.log("body in upload controller", req.body, { fileName, fileType})
     if (!fileName || !fileType || fileName.trim() === '' || fileType.trim() === '') {
-        throw new ApiError(404, "file name or key is required");
+        return res
+        .status(400)
+        .json(
+        new ApiResponse(400, null, "file name or key is required")
+        );
     }
     const objectKey = `${slugifyString(Date.now().toString())}-${slugifyString(fileName)}`;
 
