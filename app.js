34b6218867685034147d0ebed4cdaf3bfa75c13e
Mritunjay <mupadhyay00@gmail.com>
2024-05-27 19:02:30 +0700

    🔧 refactor(app.js): clean up commented out code and unused variables for better code readability 🔒 chore(app.js): implement CORS policy with allowed origins to enhance security 🚀 chore(index.js): update server start function to use 'app' instead of 'httpServer' for consistency

--- app.js
+++ app.js
@@ -6,17 +6,39 @@ import uploadRouter from "./routes/upload.routes.js";
 import customerRouter from "./routes/customer.routes.js";
 import eventRouter from "./routes/event.routes.js";
 
-import { createServer } from "http";
+// import { createServer } from "http";
 const app = express();
-const httpServer = createServer(app);
+// const httpServer = createServer(app);
 
+// List of allowed origins
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://jagota-food-festival.mjcodes.dev",
+    "https://jagota-food-festival-dev.pages.dev"
+];
+
+// CORS options
 const corsOptions = {
-    origin: process.env.CORS_ORIGIN3,
-    credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200 // For legacy browser support
 };
-console.log("CORS_ORIGIN: ", process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2, process.env.CORS_ORIGIN3);
-// global middlewares
+
+// Apply CORS middleware
 app.use(cors(corsOptions));
+
+// const corsOptions = {
+//     origin: process.env.CORS_ORIGIN3,
+//     credentials: true,
+// };
+// console.log("CORS_ORIGIN: ", process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2, process.env.CORS_ORIGIN3);
+// // global middlewares
+// app.use(cors(corsOptions));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); // configure static file to save images locally
@@ -26,4 +48,4 @@ app.use("/api/uploads", uploadRouter);
 app.use("/api/customers", customerRouter);
 app.use("/api/events", eventRouter);
 
-export { httpServer };
+export { app };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { httpServer } from "./app.js";
+import { app } from "./app.js";
 import { connectDB } from "./db/mongoose.connection.js";
 
 const startServer = () => {
-    httpServer.listen(8000, () => {
+  app.listen(8000, () => {
       console.log("⚙️  Server is running on port: " + 8000);
     });
 };
