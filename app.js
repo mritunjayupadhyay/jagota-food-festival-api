ccf521c337387260de73765b8f6098546a1a4b8f
Mritunjay <mupadhyay00@gmail.com>
2024-05-27 20:44:17 +0700

    🔧 refactor(app.js): remove commented out code and unused variables for cleaner codebase 🚀 feat(app.js): add basic routes for root path and status endpoint to improve API functionality

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ import uploadRouter from "./routes/upload.routes.js";
 import customerRouter from "./routes/customer.routes.js";
 import eventRouter from "./routes/event.routes.js";
 
-// import { createServer } from "http";
 const app = express();
-// const httpServer = createServer(app);
 
 // List of allowed origins
 const allowedOrigins = [
     "http://localhost:3000",
+    "http://127.0.0.1:5500",
     "https://jagota-food-festival.mjcodes.dev",
     "https://jagota-food-festival-dev.pages.dev"
 ];
@@ -32,16 +31,13 @@ const corsOptions = {
 // Apply CORS middleware
 app.use(cors(corsOptions));
 
-// const corsOptions = {
-//     origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2, process.env.CORS_ORIGIN3],
-//     credentials: true,
-// };
-// console.log("CORS_ORIGIN: ", process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2, process.env.CORS_ORIGIN3);
-// // global middlewares
-// app.use(cors(corsOptions));
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public")); // configure static file to save images locally
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/status", (req, res) => {
+  res.json({ message: "Server is running" });
+});
 
 app.use("/api/auth", authRouter);
 app.use("/api/uploads", uploadRouter);
