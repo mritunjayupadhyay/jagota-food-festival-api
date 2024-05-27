4f07fbc758659f91cf9ad957a146f92e47b596fb
Mritunjay <mupadhyay00@gmail.com>
2024-05-27 22:00:02 +0700

    ✨ feat(app.js): add middleware for parsing JSON and URL-encoded data with size limit of 16kb to improve security and performance

--- app.js
+++ app.js
@@ -39,6 +39,9 @@ app.get("/status", (req, res) => {
   res.json({ message: "Server is running" });
 });
 
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
 app.use("/api/auth", authRouter);
 app.use("/api/uploads", uploadRouter);
 app.use("/api/customers", customerRouter);
